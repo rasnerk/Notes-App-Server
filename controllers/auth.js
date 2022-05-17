bfaa148c96438361888b59ee4adc44db2b93ca81
Keifer Rasner <rasnerkeifer@gmail.com>
2022-05-17 10:44:03 -0500

    Auth Testing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,36 @@
+const bcrypt = require('bcrypt');
+const Crypto = require('crypto')
+const User = require('../db/schemas/user')
+
 const registerUser = (req,res) => {
-    // -- Name: username, email: email, password: password -- //
-    // get this stuff ^, then send it back to the user
+    const { email, name, password } = req.body;
+
+    User.findOne({email:email}, async (err,doc) => {
+        if (err) return res.send(err)
+        if (doc) return res.send('User Already Exists')
+  
+        const hashedPassword = await bcrypt.hash(password,10);
+        const newUser = await User.create({email: email, name: name, password: hashedPassword})
+        return newUser ? res.send(`User Created Successfully!`) : res.send('Failed to create user')
+    })
 }
 
-const login = (req,res) => {
+const login = async (req,res) => {
     // same as register
+    const { email, password } = req.body;
+    let user = await User.findOne({email: email})
+    if (!user) return res.send('Invalid Email or Password.')
+
+    let validPass = await bcrypt.compare(password, user.password)
+    if (!validPass) return res.send('Invalid Email or Password.')
+    let authToken = generateAuthToken()
+    console.log(authToken)
+    User.findByIdAndUpdate(user._id, {authToken: authToken})
+    res.send({name: user.name, uid: user._id, authToken: user.authToken})
 }
 
 const logout = (req,res) => {
-    // same as register
+    // find user by id and terminate authToken && redirect the user back to login page
 }
 
 const validateSession = (req,res) => {
@@ -16,12 +38,12 @@ const validateSession = (req,res) => {
     // send it back to the user
 }
 
-const assignSessionToken = () => {
-
+const generateAuthToken = () => {
+    return Crypto.randomBytes(64).toString('hex')
 }
 
 const terminateSessionToken = () => {
-    
+    // find user by id and remove the users authToken
 }
 
 module.exports = { registerUser, login, logout, validateSession }
---
+++ db/schemas/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        required: true,
+        type: String
+    },
+    name: {
+        required: true,
+        type: String
+    },
+    password: {
+        required: true,
+        type: String
+    },
+    authToken: {
+        type: String
+    }
+})
+
+module.exports = mongoose.model('User',userSchema);
